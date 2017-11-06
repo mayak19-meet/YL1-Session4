d3844601b34067d8068539d7287bf057aded9f4b
tamerk19-meet <tamerk19@meet.mit.edu>
2017-11-06 19:22:10 +0200

    maya:)

---
+++ lab4.py
@@ -0,0 +1,21 @@
+Class Animal (object):
+	def_init_(self,sound,name,favorit_color,favorit_food)
+		self.sound=sound
+		self.name=name
+		self.age=age
+		self.favorite_color=favorite_color
+		self.favorit_food=favorit_food
+
+	def eat(self,food):
+		print("Yummy!!" + self.name + "is eating" + favorit_food)
+
+	def description(self):
+		print(self.name + "is" + self.age +"years old an loves the color"+self.favorite_color)
+
+	def make_sound(self,sound):
+		print(self.sound*3)
+
+	def make_soundx(self,x):
+		print(self.sound*x)
+
+	c=animal("meaw,cat,red,fish")
